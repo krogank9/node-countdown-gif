7a41e49056eec9b882be505531443acbd74c2862
krogank9 <wifi.mouse.app@gmail.com>
2019-08-01 12:45:59 -0400

    up

--- countdown-generator/index.js
+++ countdown-generator/index.js
@@ -139,7 +139,7 @@ module.exports = {
                 ctx.fillRect(0, 0, this.width, this.height);
 						
 				function fillTextMultiLine(ctx, text, x, y) {
-					var lineHeight = ctx.measureText("M").width * 1.5;
+					var lineHeight = ctx.measureText("M").width * 1.2;
 					var lines = text.split("\n");
 					for (var i = 0; i < lines.length; ++i) {
 						ctx.fillText(lines[i], x, y);
