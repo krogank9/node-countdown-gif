1c0bad5f57ce9a7f0597f5f0b77b5f356f38a07a
krogank9 <wifi.mouse.app@gmail.com>
2019-08-01 12:28:07 -0400

    up

--- countdown-generator/index.js
+++ countdown-generator/index.js
@@ -139,7 +139,7 @@ module.exports = {
                 ctx.fillRect(0, 0, this.width, this.height);
 						
 				function fillTextMultiLine(ctx, text, x, y) {
-					var lineHeight = ctx.measureText("M").width * 1.2;
+					var lineHeight = ctx.measureText("M").width * 1.5;
 					var lines = text.split("\n");
 					for (var i = 0; i < lines.length; ++i) {
 						ctx.fillText(lines[i], x, y);
@@ -148,7 +148,7 @@ module.exports = {
 				}
 				
 				// set font style
-				let fontSize = Math.floor(this.width / 8) + 'px';
+				let fontSize = Math.floor(this.width / 10) + 'px';
 				let fontFamily = 'Courier New'; // monospace works slightly better
 				ctx.font = [fontSize, fontFamily].join(' ');
                 
