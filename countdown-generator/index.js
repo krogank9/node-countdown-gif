e25287878fe3a28f1a0081ddac93e9e8ded61798
krogank9 <wifi.mouse.app@gmail.com>
2019-08-06 12:39:20 -0400

    up

--- countdown-generator/index.js
+++ countdown-generator/index.js
@@ -86,8 +86,6 @@ module.exports = {
         let tmpDir = process.cwd() + '/tmp/';
 		
 		enc.setTransparent(0xFF00FF);
-		ctx.fillStyle = 0xFF00FF;
-		ctx.fillRect(0,0,this.width,this.height)
 
         // create the tmp directory if it doesn't exist
         if (!fs.existsSync(tmpDir)){
@@ -140,8 +138,8 @@ module.exports = {
                 let string = [parseInt(days)>0?days:"", parseInt(days)?' days, ':"", parseInt(hours)>0?hours:"", parseInt(hours)>0?' hours\n':"", minutes, ' minutes, ', seconds, ' seconds'].join('');
                 
                 // paint BG
-                ctx.fillStyle = this.bg;
-                //ctx.fillRect(0, 0, this.width, this.height);
+                ctx.fillStyle = "#ff00ff";
+                ctx.fillRect(0, 0, this.width, this.height);
 						
 				function fillTextMultiLine(ctx, text, x, y) {
 					var lineHeight = ctx.measureText("M").width * 1.5;
